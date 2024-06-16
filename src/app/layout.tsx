02c5e79ecfbcab69f124280ec8868dce23686c47
Ihor Cherniaiev <ihorcherniaiev@Ihors-MacBook-Pro.local>
2024-06-16 18:21:28 +0300

    Init shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,15 @@
 import type { Metadata } from 'next';
+import { Inter as FontSans } from 'next/font/google';
+
+import { ThemeProvider } from '@/components/theme-provider';
+import { cn } from '@/lib/utils';
+
 import './globals.css';
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
   title: 'DeQuiz',
@@ -11,8 +21,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
+    <>
+      <html lang='en' suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            'bg-background min-h-screen bg-zinc-950 font-sans antialiased',
+            fontSans.variable
+          )}
+        >
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   );
 }
