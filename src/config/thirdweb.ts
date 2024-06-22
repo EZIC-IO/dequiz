29ba998f3cbfaf6b2acbe1333e4d0f7de4a07094
igorekch1 <ihor.cherniaiev@gmail.com>
2024-06-22 04:26:15 +0300

    Update contract address

--- src/config/thirdweb.ts
+++ src/config/thirdweb.ts
@@ -1,6 +1,6 @@
 import { createThirdwebClient } from 'thirdweb';
 
-export const contractAddress = '0x7c5F081a98fd78665F8cEAD1863E75E8619D3f85';
+export const contractAddress = '0x0Ddb253Dee9de334aE831d1757a2833256ee0Ff2';
 
 const clientId = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
 
